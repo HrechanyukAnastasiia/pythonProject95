381fdcc95ff0cf94bb426e895d5a7b082295739f
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-31 18:05:29 +0300

    3 завдання

--- 2.06.py
+++ 2.06.py
@@ -9,11 +9,11 @@ def generate_numbers(file_path, n):
     try:
         with open(file_path, "w") as file:
             for i in range(n):
-                random_number = random.randint(1, 100)
+                random_number = random.randint(1, 200)
                 file.write(str(random_number) + '\n')
                 logging.info(f"Рандомне число: {random_number}")
     except Exception as e:
         logging.error(f"Error: {e}")
 file_path = "numbers.txt"
-num_numbers = 6
+num_numbers = 4
 generate_numbers(file_path, num_numbers)
